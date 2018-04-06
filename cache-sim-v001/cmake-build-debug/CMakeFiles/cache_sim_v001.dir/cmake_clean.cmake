1cf8ce0225f0f861465d04154ee3330c452867c9
CUTE-KERNEL <36137609+CUTE-KERNEL@users.noreply.github.com>
2018-04-06 18:50:12 -0400

    Update-HWK3
    running trace0, 2 and 3

---
+++ cache-sim-v001/cmake-build-debug/CMakeFiles/cache_sim_v001.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cache_sim_v001.dir/cachesimulator.cpp.obj"
+  "cache_sim_v001.pdb"
+  "cache_sim_v001.exe"
+  "cache_sim_v001.exe.manifest"
+  "libcache_sim_v001.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cache_sim_v001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
